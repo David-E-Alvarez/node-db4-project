8766ed0ed97e7d54a81cbe49b720f97352567ceb
david <davidealvarez1@gmail.com>
2020-05-14 14:41:03 -0700

    created tables and seeded a cleaner

--- data/migrations/20200514131925_create-recipes.js
+++ data/migrations/20200514131925_create-recipes.js
@@ -1,8 +1,27 @@
 
 exports.up = function(knex) {
-  
+    return knex.schema
+        .createTable('recipes', tbl => {
+            tbl.increments('id');
+            tbl.string('name').notNullable();
+            tbl.text('description');
+            tbl.integer('num_of_ingredients');
+            tbl.string('ingredient');
+            tbl.float('qty_of_ingredient_in_rec');
+            tbl.text('instructions');
+        })
+        .createTable('ingredients', tbl => {
+            tbl.increments();
+            tbl.string('name').notNullable();
+            tbl.text('description');
+            tbl.integer('recipe_id')
+                .references('id')
+                .inTable('recipes');
+        })
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema
+        .dropTableIfExists('ingredients')
+        .dropTableIfExists('recipes');
 };
